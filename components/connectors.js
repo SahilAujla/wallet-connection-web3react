b02eb80d0393350d05ebc870e2b49862c6a04b15
Sahil Aujla <sahilaujla15@gmail.com>
2022-04-11 22:23:36 +0530

    Added the support for walletconnect.

--- components/connectors.js
+++ components/connectors.js
@@ -1,3 +1,21 @@
+// import { InjectedConnector } from "@web3-react/injected-connector";
+// import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
+
+// const injected = new InjectedConnector({
+//   supportedChainIds: [1, 3, 4, 5, 42]
+// });
+
+// const walletconnect = new WalletConnectConnector({
+//   rpcUrl: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+//   bridge: "https://bridge.walletconnect.org",
+//   qrcode: true
+// });
+
+// export const connectors = {
+//   injected: injected,
+//   walletConnect: walletconnect,
+// };
+
 import { InjectedConnector } from "@web3-react/injected-connector";
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 
@@ -5,9 +23,25 @@ const injected = new InjectedConnector({
   supportedChainIds: [1, 3, 4, 5, 42]
 });
 
+
+
+const ALL_SUPPORTED_CHAIN_IDS = [1, 2, 4]
+
+const INFURA_NETWORK_URLS = {
+  1: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+  2: `https://ropsten.infura.io/v3/${process.env.INFURA_KEY}`,
+  4: `https://rinkeby.infura.io/v3/${process.env.INFURA_KEY}`
+}
+
+// const ALL_SUPPORTED_CHAIN_IDS = {
+
+// }
+
 const walletconnect = new WalletConnectConnector({
-  rpcUrl: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
-  bridge: "https://bridge.walletconnect.org",
+  // rpcUrl: https://rinkeby.infura.io/v3/${infuraId},
+  supportedChainIds: ALL_SUPPORTED_CHAIN_IDS,
+  rpc: INFURA_NETWORK_URLS,
+  bridge: "https://bridge.walletconnect.org/",
   qrcode: true
 });
 
