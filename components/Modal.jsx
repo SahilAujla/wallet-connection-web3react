b52226888ad63e0cf434aa3295ddf590f875c93a
Sahil Aujla <sahilaujla15@gmail.com>
2022-04-12 08:30:49 +0530

    Added reload.

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -74,6 +74,7 @@ import {
                   await activate(connectors.walletConnect);
                   setProvider("walletConnect");
                   closeModal();
+                  document.location.reload();
                 }}
                 w="100%"
               >
